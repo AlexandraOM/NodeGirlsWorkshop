06d5293d3f12b903cf3132f5e7fd123621b94b84
Alex O'M <alex@Alex-OMs-MacBook-Pro.local>
2019-08-31 12:40:09 +0100

    adding comments and readability

--- server.js
+++ server.js
@@ -1,56 +1,54 @@
-var express = require('express');
+var express = require("express");
 var formidable = require("express-formidable");
-var fs = require('fs');
+var fs = require("fs");
 var app = express();
-
 
 // app.get("/", function(req, res) {
 //   res.send("Yay Node Girls!");
 // });
 
 // app.get("/node", function(req, res) {
-//   res.send("Send nodes")
+//   res.send("Send nodes");
 // });
 //
 // app.get("/chocolate", function(req, res) {
-//   res.send("Mm chocolate :O")
+//   res.send("Mm chocolate :O");
 // });
 //
 // app.get("/girls", function(req, res) {
-//   res.send("Day 1 of building my blog")
+//   res.send("just want to have FUNdamental rights");
 // });
 
-
-
-
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
 
 app.use(formidable());
 
-
-app.post('/create-post', function(req, res) {
+app.post("/create-post", function(req, res) {
   console.log(req.fields);
-  fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-      console.log(file);
-      console.log(file.toString());
-      var allPosts = JSON.parse(file);
-      var newPost = req.fields.blogpost;
-      var timestamp = Date.now()
-      console.log(timestamp)
-        allPosts[timestamp] = newPost
-        var allPostsWords = JSON.stringify(allPosts)
-        fs.writeFile(__dirname + '/data/posts.json', allPostsWords, function (error, file) {
-          console.log('Finished?')
-
-      })
-
+  //Argument 1: the location of the file you want to write to
+  fs.readFile(__dirname + "/data/posts.json", function(error, file) {
+    console.log(file);
+    console.log(file.toString());
+    var parsedFile = JSON.parse(file);
+    //Argument 2: the data you want to write
+    var newPost = req.fields.blogpost;
+    var timestamp = Date.now();
+    console.log(timestamp);
+    parsedFile[timestamp] = newPost;
+    var allPostsWords = JSON.stringify(parsedFile);
+    fs.writeFile(__dirname + "/data/posts.json", allPostsWords, function(
+      error,
+      file
+    ) {
+      console.log(req.fields);
+    });
   });
+});
 
-  console.log(req.body);
-    res.send('Hello there!');
+app.get("/get-posts", function(req, res) {
+  res.sendFile(__dirname + "/data/posts.json");
 });
 
 app.use(express.static("public"));
-
-app.listen(3000, function() {
-  console.log('Server is listening on port 3000. Ready to accept requests!');
-})
